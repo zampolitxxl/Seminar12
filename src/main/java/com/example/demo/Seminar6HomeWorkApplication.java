5f1315602f670872e471c56663d0034075ea167b
kirilltimofeev <sushi-maker@yandex.ru>
2024-05-10 19:07:04 +0300

    Seminar9HomeWorkDone This is base project

--- src/main/java/com/example/demo/Seminar6HomeWorkApplication.java
+++ src/main/java/com/example/demo/Seminar6HomeWorkApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Seminar6HomeWorkApplication {
@@ -11,4 +14,13 @@ public class Seminar6HomeWorkApplication {
 
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("Microservice1",r->r.path("/serviceA/**")
+						.uri("http://localhost:8081/"))
+				.route("Microservice2",r->r.path("/serviceB/**")
+						.uri("http://localhost:8082/")).build();}
 }
+
+
